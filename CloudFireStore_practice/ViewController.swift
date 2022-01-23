5d34bc8d8b26395a9f10d40f3ce15e646b252d66
Masato Yasuda <yasudaM84@gmail.com>
2022-01-23 23:36:24 +0900

    データ追加項目をユーザーからコメントに変更

--- CloudFireStore_practice/ViewController.swift
+++ CloudFireStore_practice/ViewController.swift
@@ -18,28 +18,25 @@ final class ViewController: UIViewController {
     }
   }
   @IBOutlet private weak var textField: UITextField!
-  private var personCount: Int = 0
+  private let db = Firestore.firestore()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
   @IBAction func didTapAddButton(_ sender: Any) {
-    guard let name = textField.text, !name.isEmpty else { return }
+    guard let text = textField.text, !text.isEmpty else { return }
     textField.text = ""
-    personCount += 1
-    let person = PersonModel(name: name)
-    let db = Firestore.firestore()
-    do {
-      try db.collection("Person").document("person\(personCount)").setData(from: person)
-    } catch let error {
-      print("Firestore error: \(error)")
+    db.collection("post").addDocument(data: ["text": text]) { error in
+      if let error = error {
+        print("Error adding document: \(error)")
+      }
     }
   }
 
   @IBAction func didTapUpdateButton(_ sender: Any) {
 
+    tableView.reloadData()
   }
 }
 
@@ -48,14 +45,12 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController: UITableViewDataSource {
-
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     1
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = "text"
     return cell
   }
 }
